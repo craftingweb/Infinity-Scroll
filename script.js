3f9b6be06362d6aab25ec937d1c1a8d45e0b3d03
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-02-05 21:17:03 -0500

    code review, ready for production

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
 
-let ready = false; // when page first load it will be false;
-let imagesLoaded = 0; // how many images are loaded (30)
-let totalImages = 0; // keep tracking all images that's why we will know when if it finish loading
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 
 let photosArray = [];
 
@@ -16,11 +16,10 @@ const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&coun
 // Check if all images are loaded
 function imageLoaded() {
   imagesLoaded++;
-  // console.log(imagesLoaded);
+
   if (imagesLoaded === totalImages) {
     ready = true;
     loader.hidden = true;
-    // console.log("ready", ready);
   }
 }
 // Helper function to set attributes for DOM Elements
@@ -34,22 +33,18 @@ function setAttributes(element, atributes) {
 function displayPhotos() {
   imagesLoaded = 0;
   totalImages = photosArray.length;
-  // console.log("total images", totalImages);
-  // Run function for each object (element)
+
   photosArray.forEach((photo) => {
     // create <a> to link to Unsplash
     const item = document.createElement("a");
-    // item.setAttribute("href", photo.links.html);
-    // item.setAttribute("target", "_blank");
+
     setAttributes(item, {
       href: photo.links.html,
       target: "_blank",
     });
     // create <img> for photo
     const img = document.createElement("img");
-    // img.setAttribute("src", photo.urls.regular);
-    // img.setAttribute("alt", photo.alt_description);
-    // img.setAttribute("title", photo.alt_description);
+
     setAttributes(img, {
       src: photo.urls.regular,
       alt: photo.alt_description,
@@ -81,10 +76,7 @@ window.addEventListener("scroll", () => {
     ready = false;
     getPhotos();
   }
-  // console.log("scrolled");
-  // console.log(window.innerHeight);
-  // console.log(window.scrollY);
-  // console.log(document.body.offsetHeight);
 });
+
 // On load
 getPhotos();
